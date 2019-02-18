b32353e5fba482ef64defa8f25ae6cce1722a4b9
pzupke3 <pzupke3@gatech.edu>
2019-02-17 22:57:58 -0500

    Added basic implementation for moving magnet in a straight line

--- Wreckmanji/Wreckmanji.ino
+++ Wreckmanji/Wreckmanji.ino
@@ -15,14 +15,22 @@ typedef enum State {
   UP,
   DOWN
 } states;
+//these are the coordinates of the final destination when moving in a straight line
+double final_x;
+double final_y;
 //these are the coordinates you want to get to
-int x;
-int y;
+double x;
+double y;
 //the current x and y coordinates of the servos
-int curr_x;
-int curr_y;
+double curr_x;
+double curr_y;
+//used to keep track of time when moving in a line
+double timer;
+//the new angles we want the servos to move to
 int new_top_angle;
 int new_bottom_angle;
+//true if we want the magnet to move in a straight line
+bool linear_movement;
 State state;
 void setup() {
   // put your setup code here, to run once:
@@ -32,8 +40,10 @@ void setup() {
   curr_y = 0;
   x = 100;
   y = 100;
-  int new_top_angle = 90;
-  int new_bottom_angle = 90;
+  new_top_angle = 90;
+  new_bottom_angle = 90;
+  linear_movement = false;
+  timer = 0;
   state = WAITING;
 }
 
@@ -43,14 +53,21 @@ void loop() {
   switch (state) {
     //calculate the angle you need to move to and set state to movement
     case WAITING:
+      if (linear_movement) {
+        if (final_x != curr_x || final_y != curr_y) {
+          timer += 0.1;
+          x = LERP(curr_x, final_x, timer);
+          y = LERP(curr_y, final_y, timer);
+        }
+      }
       if (x != curr_x || y != curr_y) {
         if (x > 0) {
           new_top_angle = (int) 1.5 * PI - acos((pow(x, 2) + pow(y, 2) - pow(top_arm, 2) - pow(bottom_arm, 2))/(2 * bottom_arm*top_arm));
-          new_bottom_angle = (int) (atan((float) y/x) - atan(top_arm*cos(new_top_angle)/(bottom_arm-top_arm*sin(new_top_angle)));
+          new_bottom_angle = (int) (atan((float) y/x) - atan(top_arm*cos(new_top_angle)/(bottom_arm-top_arm*sin(new_top_angle))));
           
         } else {
           new_top_angle = (int) acos((pow(x, 2) + pow(y, 2) - pow(top_arm, 2) - pow(bottom_arm, 2))/(2 * bottom_arm*top_arm) - PI/2);
-          new_bottom_angle = (int) (PI + atan((float) y/x) - atan(top_arm*cos(new_top_angle)/(bottom_arm-top_arm*sin(new_top_angle)));
+          new_bottom_angle = (int) (PI + atan((float) y/x) - atan(top_arm*cos(new_top_angle)/(bottom_arm-top_arm*sin(new_top_angle))));
         }
       }
       state = MOVEMENT;
@@ -64,7 +81,8 @@ void loop() {
       curr_y = y;
       state = WAITING;
   }
-      
-    
-  
+}
+
+double LERP(double curr_x, double x, double t) {
+  return curr_x + t * (x - curr_x);
 }
