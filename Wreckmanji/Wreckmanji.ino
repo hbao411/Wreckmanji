4f3016cd6c33af84baf05197ecc1b70ee15a75eb
pzupke3 <pzupke3@gatech.edu>
2019-03-06 13:38:37 -0500

    Added linearization

--- Wreckmanji/Wreckmanji.ino
+++ Wreckmanji/Wreckmanji.ino
@@ -10,6 +10,7 @@ const int top_arm = 100;
 typedef enum State {
   WAITING,
   MOVEMENT,
+  END,
   LEFT,
   RIGHT,
   UP,
@@ -35,22 +36,27 @@ int new_bottom_angle;
 bool linear_movement;
 State state;
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   bottomServo.attach(servo1);
   topServo.attach(servo2);
   curr_x = 0;
   curr_y = 0;
-  x = 100;
-  y = 100;
+  x = 0;
+  y = 0;
   new_top_angle = 90;
   new_bottom_angle = 90;
-  linear_movement = false;
+  linear_movement = true;
   timer = 0;
   state = WAITING;
-  //start_x = 0;
-  //start_y = 0;
-  //final_x = 100;
-  //final_y = 100;
+  start_x = 0;
+  start_y = 0;
+  final_x = 50;
+  final_y = 50;
+  bottomServo.write(90);
+  delay(500); 
+  topServo.write(90);
+  delay(500);
 }
 
 void loop() {
@@ -61,12 +67,17 @@ void loop() {
     case WAITING:
       if (linear_movement) {
         if (final_x != curr_x || final_y != curr_y) {
-          timer += 0.1;
+          if (timer != 1) {
+            timer += 0.05;
+          } else {
+            exit(0);
+          }
+          Serial.println(timer);
           x = LERP(start_x, final_x, timer);
           y = LERP(start_y, final_y, timer);
         }
       }
-      if (x != curr_x || y != curr_y) {
+      //if (x != curr_x || y != curr_y) {
         if (x > 0) {
           new_top_angle = (int) ((1.5 * PI - acos((pow(x, 2) + pow(y, 2) - pow(top_arm, 2) - pow(bottom_arm, 2))/(2 * bottom_arm*top_arm))) * 180 / PI);
           new_bottom_angle = (int) ((atan((float) y/x) - atan(top_arm*cos(new_top_angle)/(bottom_arm-top_arm*sin(new_top_angle)))) * 180 / PI);
@@ -75,17 +86,29 @@ void loop() {
           new_top_angle = (int) ((acos((pow(x, 2) + pow(y, 2) - pow(top_arm, 2) - pow(bottom_arm, 2))/(2 * bottom_arm*top_arm) - PI/2)) * 180 / PI);
           new_bottom_angle = (int) ((PI + atan((float) y/x) - atan(top_arm*cos(new_top_angle)/(bottom_arm-top_arm*sin(new_top_angle)))) * 180 / PI);
         }
+      //}
+      
+      if (timer == 1) {
+        state = END;
+      } else {
+        state = MOVEMENT;
       }
-      state = MOVEMENT;
       break;
     case MOVEMENT:
-      bottomServo.write(new_bottom_angle);
-      delay(500);
-      topServo.write(new_top_angle);
-      delay(500);
-      curr_x = x;
-      curr_y = y;
-      state = WAITING;
+      if (timer != 1) {
+        bottomServo.write(new_bottom_angle);
+        delay(100); 
+        topServo.write(new_top_angle);
+        delay(100);
+        curr_x = x;
+        curr_y = y;
+        state = WAITING;
+      } else {
+        state = END;
+      }
+      break;
+    case END:
+      exit(0);
   }
 }
 
